ac2027ef6eb28659f1c87eb37992107ddb0a4f82
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2024-01-09 22:14:16 +0900

    #18 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    강의 시간 상 기능의 일부만 테스트로 표현함
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ project-board/src/main/java/com/example/projectboard/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.dto.ArticleCommentDto;
+import com.example.projectboard.repository.ArticleCommentRepository;
+import com.example.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
