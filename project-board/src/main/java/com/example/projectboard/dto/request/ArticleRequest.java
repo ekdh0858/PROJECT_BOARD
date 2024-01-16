c642a87bb97263030ccfe6f1bcf03a759dadd1bb
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2024-01-16 23:28:17 +0900

    #35 - 게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현

---
+++ project-board/src/main/java/com/example/projectboard/dto/request/ArticleRequest.java
@@ -0,0 +1,25 @@
+package com.example.projectboard.dto.request;
+
+import com.example.projectboard.dto.ArticleDto;
+import com.example.projectboard.dto.UserAccountDto;
+
+public record ArticleRequest(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleRequest of(String title, String content, String hashtag) {
+        return new ArticleRequest(title, content, hashtag);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtag
+        );
+    }
+
+}
