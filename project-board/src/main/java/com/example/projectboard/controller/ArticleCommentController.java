6447429109a3ae68a4c325b39486d321b01cccec
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2024-01-17 11:30:19 +0900

    #33 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

---
+++ project-board/src/main/java/com/example/projectboard/controller/ArticleCommentController.java
@@ -0,0 +1,37 @@
+package com.example.projectboard.controller;
+
+import com.example.projectboard.dto.UserAccountDto;
+import com.example.projectboard.dto.request.ArticleCommentRequest;
+import com.example.projectboard.service.ArticleCommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+@Controller
+public class ArticleCommentController {
+
+    private final ArticleCommentService articleCommentService;
+
+    @PostMapping ("/new")
+    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest) {
+        // TODO: 인증 정보를 넣어줘야 한다.
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
+                "uno", "pw", "uno@mail.com", null, null
+        )));
+
+
+        return "redirect:/articles/" + articleCommentRequest.articleId();
+    }
+
+    @PostMapping ("/{commentId}/delete")
+    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
+        articleCommentService.deleteArticleComment(commentId);
+
+        return "redirect:/articles/" + articleId;
+    }
+
+}
